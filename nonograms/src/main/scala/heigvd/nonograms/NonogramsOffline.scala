747107ecd0122539342448b34bd0398ed59f5f1f
paranoodle <paranoodle@users.noreply.github.com>
2016-05-25 18:58:57 +0200

    grid display test works

--- nonograms/src/main/scala/heigvd/nonograms/NonogramsOffline.scala
+++ nonograms/src/main/scala/heigvd/nonograms/NonogramsOffline.scala
@@ -13,10 +13,26 @@ object NonogramsOffline extends ScageScreenApp("Scage App", 640, 480) {
     ang += 5
   }
 
-  backgroundColor = BLACK
+  backgroundColor = WHITE
+  
+  val grid = List(List(RED, YELLOW), List(GREEN, BLUE))
+  val sizeX = 2
+  val sizeY = 2
+  val gridSize = 15
+  
   render {
-    openglMove(windowSize/2)
-    openglRotate(ang)
-    print("Hello World!", Vec(-50, -5), GREEN)
+    for (x <- 0 to sizeX) {
+      val pos = 100 + (sizeX - x) * gridSize
+      drawLine(Vec(100, pos), Vec(100 + gridSize * sizeY, pos), GRAY)
+    }
+    for (y <- 0 to sizeY) {
+      val pos = 100 + (sizeY - y) * gridSize
+      drawLine(Vec(pos, 100), Vec(pos, 100 + gridSize * sizeX), GRAY)
+    }
+    for (x <- 0 until sizeX; y <- 0 until sizeY) {
+      val posX = 100 + 7 + ((y) * (gridSize))
+      val posY = 100 - 7 + ((sizeX - x) * (gridSize))
+      drawFilledRectCentered(Vec(posX, posY), 10, 10, grid(x)(y))
+    }
   }
 }
